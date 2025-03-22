ca2d8dae7acb1477ee25a6a7bad28de06fa11c76
Jeff Langr <jeff@langrsoft.com>
2025-03-21 20:16:43 -0600

    pipeline

--- src/students/model/nameNormalizer.pipeline.mjs
+++ src/students/model/nameNormalizer.pipeline.mjs
@@ -1,6 +1,7 @@
 export const normalizeName = name =>
   name
     |> trimName
+    |> handleSuffix
     |> splitName
     |> initializeMiddleNames
     |> formatParsedName
@@ -8,12 +9,18 @@ export const normalizeName = name =>
 const trimName = str =>
   str.trim().replace(/\s+/g, ' ')
 
-const splitName = str => {
-  const parts = str.split(' ')
+const handleSuffix = str => {
+  const [namePart, suffix] = str.split(',')
+  return { name: namePart, suffix: suffix ? suffix.trim() : '' }
+}
+
+const splitName = ({ name, suffix }) => {
+  const parts = name.split(' ')
   return {
     first: parts[0] || '',
     middle: parts.slice(1, -1),
-    last: parts.length > 1 ? parts[parts.length - 1] : ''
+    last: parts.length > 1 ? parts[parts.length - 1] : '',
+    suffix
   }
 }
 
@@ -23,7 +30,7 @@ const initializeMiddleNames = parsed =>
     middle: parsed.middle.map(n => `${n[0]}.`)
   })
 
-const formatParsedName = ({ first, middle, last }) =>
+const formatParsedName = ({ first, middle, last, suffix }) =>
   !last
     ? first
-    : `${last}, ${first}${middle.length ? ' ' + middle.join(' ') : ''}`
+    : `${last}, ${first}${middle.length ? ' ' + middle.join(' ') : ''}${suffix ? ', ' + suffix : ''}`
